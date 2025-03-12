ce252f7c01f5a70549a18bff587a690ef100334b
Gilgamesh-hoang <vophihoang252003@gmail.com>
2025-03-12 09:39:29 +0700

    initialize the project

---
+++ src/main/java/org/cdwbackend/CdwBackendApplication.java
@@ -0,0 +1,13 @@
+package org.cdwbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdwBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdwBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cdwbackend/CdwBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cdwbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdwBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
