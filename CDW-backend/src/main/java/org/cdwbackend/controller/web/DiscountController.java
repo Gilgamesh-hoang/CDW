18b1ce848d9904aa65e96b227e9b32aebbd047a7
hoanghuydev <tranvohoanghuy12ab@gmail.com>
2025-06-18 04:33:30 +0700

    feat: get products

--- CDW-backend/src/main/java/org/cdwbackend/controller/web/DiscountController.java
+++ CDW-backend/src/main/java/org/cdwbackend/controller/web/DiscountController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController("DiscountController")
 @RequestMapping("/api/discounts")
 @RequiredArgsConstructor
 public class DiscountController {
